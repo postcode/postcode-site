b5bafd32d60da0ffa858cab1046ab97a0a63f9e1
CJ Bryan <christianbryan@gmail.com>
2014-01-29 12:15:11 -0800

    Pluck the environment variable for a Google Analytics code and pass it on to the views.

--- postcode/views.py
+++ postcode/views.py
@@ -4,15 +4,17 @@
 from flask import render_template, url_for, redirect
 from postcode import app
 from jinja_filters import *
+import os
+
+ga_tracking_code = os.getenv('GA_TRACKING_CODE', 'Not defined!')
 
 # Submitting a new request
 def index():
-	return render_template('index.html')
-
+	return render_template('index.html', ga_tracking_code = ga_tracking_code)
 
 def any_page(page):
 	try:
-		return render_template('%s.html' %(page))
+		return render_template('%s.html' %(page), ga_tracking_code = ga_tracking_code)
 	except:
 		return index()
 
