786146edd6919d69e88df858353e7376d6527a04
Richa Agarwal <richa@Richas-MacBook-Air.local>
2014-05-28 15:06:03 -0700

    Talk to RecordTrac API to get num results back

--- postcode/jinja_filters.py
+++ postcode/jinja_filters.py
@@ -1,3 +1,4 @@
 from postcode import helpers, app
 # Register your filter here!
 app.jinja_env.filters['get_product'] = helpers.get_product
+app.jinja_env.filters['get_api_stats'] = helpers.get_api_stats
