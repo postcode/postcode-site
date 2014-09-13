be1eaf6c3847ab34735c2162c410ec7e2a7cd142
andyhull <andyhull3@gmail.com>
2014-09-13 15:53:48 -0400

    added sign up confirmation page. Upgraded Flask to v0.10

--- postcode/__init__.py
+++ postcode/__init__.py
@@ -7,13 +7,13 @@
 from os import environ
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from database import init_db, db_session
+# from database import init_db, db_session
 
 # Initialize Flask app 
 app = Flask(__name__)
 app.debug = True
 app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 app.config['SECRET_KEY'] = environ['SECRET_KEY']
 
-init_db()
+# Initialize database
+db = SQLAlchemy(app)
--- postcode/static/js/custom.js
+++ postcode/static/js/custom.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
+
+
+$('#rt-signup').click(function(){
+  var email = $('#email').val();
+  var name = $('#name').val();
+  var newSignup = $.ajax({
+    data: {test: email},
+    type: 'post',
+    url: '/recordtrac',
+    contentType: 'application/json;charset=UTF-8',
+  })
+  newSignup.done(function( msg ) {
+    console.log(newSignup)
+    alert(newSignup)
+  });
+})
+
 });
